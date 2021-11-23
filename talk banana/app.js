d6efa4dddc77622bfdc3677f3732a217068bec06
Noobiee12 <91060367+Noobiee12@users.noreply.github.com>
2021-11-23 23:10:38 +0530

    added button nd txtarea

--- talk banana/app.js
+++ talk banana/app.js
@@ -1,2 +1,13 @@
-var button=document.querySelector("#btn-translate")
-console.log(button)
+var button=document.querySelector("#btn-translate");
+var input=document.querySelector("txt-input");
+
+console.log(input);
+
+function clickHandler(){
+    console.log("clicked");
+    console.input("input",input.value);
+}
+
+button.addEventListener("click", clickHandler)
+
+
